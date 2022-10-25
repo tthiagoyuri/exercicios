a865f152e4bc178597a86ab66c89781f89264bb1
thiago yuri <111469803+tthiagoyuri@users.noreply.github.com>
2022-10-25 17:54:29 -0300

    Add files via upload

---
+++ list1Ex2.cpp
@@ -0,0 +1,11 @@
+#include<stdio.h>
+#include<math.h>
+int main()
+{
+ float area, raio;
+  printf("digite o valor de raio \n");
+  scanf("%f", &raio);
+  area = 3.14 * raio,2;
+  printf("area: %0.2f" , area);
+}
+
