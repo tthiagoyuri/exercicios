603e7d3945abf8e669ec39a22353ce151d0c6bcc
thiago yuri <111469803+tthiagoyuri@users.noreply.github.com>
2022-10-26 19:26:28 -0300

    Update lis1Ex3.c

--- lis1Ex3.c
+++ lis1Ex3.c
@@ -4,20 +4,21 @@ int main()
 
 {
 
-float nota1, nota2, nota3;
+float num1, num2, num3;
 float media;
 
-printf("Digite a primeira nota \n");
-scanf("%f", &nota1);
+printf("Digite o primeiro numero \n");
+scanf("%f", &num1);
 
-printf("Digite a segunda nota \n");
-scanf("%f", &nota2);
+printf("Digite o segundo numero \n");
+scanf("%f", &num2);
 
-printf("Digite a terceira nota \n");
-scanf("%f", &nota3);
+printf("Digite o terceiro numero \n");
+scanf("%f", &num3);
 
-media = (nota1 + nota2 + nota3) /3;
+media = (num1 + num2 + num3) /3;
 
 printf("A media aritmetica e %.2f :" , media);
 
 }
+
