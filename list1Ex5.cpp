6617f93ce19bbf74bfe47ce353a97047c43a5670
thiago yuri <111469803+tthiagoyuri@users.noreply.github.com>
2022-10-31 15:54:32 -0300

    Add files via upload

---
+++ list1Ex5.cpp
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+int main ()
+{
+
+int rolos; 
+int fios, metros;
+
+printf("Digite o valor de metros\n");
+scanf ("%d", &metros),
+
+rolos = metros / 50;
+fios = metros % 50;
+
+printf("Com base nos dados fornecidos a qunatidade de rolos nescessarios para a obra sera de %d rolos \n", rolos);
+
+printf("E sera necessario %d metros de fios a mais \n", fios);
+	
+	
+	
+	
+	
+	
+	
+	
+	
+}
